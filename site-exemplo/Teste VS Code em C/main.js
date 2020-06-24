ceeb7fa467968987820821a28ea0dc8ce97f26cc
Hugo Esteves Rezende <hugo.esteves@unesp.br>
2020-06-24 14:02:55 -0300

    Primeiro dia

---
+++ site-exemplo/Teste VS Code em C/main.js
@@ -0,0 +1,13 @@
+const frasesPossiveis = [
+    'Seu argumento é fraco, lhe falta lua de queijo.',
+    'Seu argumento é fraco, lhe falta lua de queijo.',
+    'Seu argumento é fraco, lhe falta lua de queijo.',
+    'Seu argumento é fraco, lhe falta lua de queijo.',
+    'Seu argumento é fraco, lhe falta lua de queijo.',
+    'Seu argumento é fraco, lhe falta lua de queijo'
+]
+function ranomizandoFrases() {
+    frasesPossiveis[] = Math.floor(Math.random() * arrayFormaProposicional.length)
+}
+let tamanhoArray = frasesPossiveis.lenght
+console.log(tamanhoArray)
